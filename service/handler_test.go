116200e29b3a5c229cdab6e432f240235f818359
Brett Adcock <brettadcock@ruelala.com>
2017-08-17 14:58:22 -0400

    added tests for formatPath

--- service/handler_test.go
+++ service/handler_test.go
@@ -6,6 +6,21 @@ import (
 
 var p Presentation
 
-func TestAddSKU(t *testing.T) {
+func TestFormatPath(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"/addSKU/id=4?name=brett?vend=rue?amt=2", "id=4;name=brett;vend=rue;amt=2"},
+		{"/updateSKU/id=4?amt=10", "id=4;amt=10"},
+		{"/deleteSKU/id=1000", "id=1000"},
+		{"/printSKUs/", ""},
+	}
 
+	for _, c := range tests {
+		actual := formatPath(c.input)
+		if actual != c.expected {
+			t.Errorf("Actual: %s\nExpected: %s", actual, c.expected)
+		}
+	}
 }
