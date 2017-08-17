7d79efa2f8828f61a132ad2f894dde9aef28e52f
Brett Adcock <brettadcock@ruelala.com>
2017-08-17 14:32:31 -0400

    refactored

--- service/handler.go
+++ service/handler.go
@@ -14,41 +14,6 @@ type Presentation struct {
 	logic logic.Logic
 }
 
-func buildSKU(vals url.Values) logic.SKU {
-	var sku logic.SKU
-	sku.ID, _ = strconv.Atoi(vals.Get("id"))
-	sku.Name = vals.Get("name")
-	sku.Vendor = vals.Get("vend")
-	sku.Quantity, _ = strconv.Atoi(vals.Get("amt"))
-
-	return sku
-}
-
-func formatPath(uri string) string {
-	rmPrefix, result := "", ""
-	numSlash := 0
-
-	for i, r := range uri {
-		c := string(r)
-		if c == "/" {
-			numSlash++
-		}
-		if numSlash == 2 {
-			rmPrefix = uri[i+1:]
-			break
-		}
-	}
-	for _, r := range rmPrefix {
-		c := string(r)
-		if c == "?" {
-			result += ";"
-		} else {
-			result += c
-		}
-	}
-	return result
-}
-
 func (p *Presentation) addSKU(res http.ResponseWriter, req *http.Request) {
 	u := formatPath(req.RequestURI)
 	values, _ := url.ParseQuery(u)
@@ -99,4 +64,39 @@ func (p *Presentation) getSKU(res http.ResponseWriter, req *http.Request) {
 	res.WriteHeader(http.StatusOK)
 	output := p.logic.GetProductInfo(sku)
 	fmt.Fprintf(res, "%s", output)
+}
+
+func buildSKU(vals url.Values) logic.SKU {
+	var sku logic.SKU
+	sku.ID, _ = strconv.Atoi(vals.Get("id"))
+	sku.Name = vals.Get("name")
+	sku.Vendor = vals.Get("vend")
+	sku.Quantity, _ = strconv.Atoi(vals.Get("amt"))
+
+	return sku
+}
+
+func formatPath(uri string) string {
+	rmPrefix, result := "", ""
+	numSlash := 0
+
+	for i, r := range uri {
+		c := string(r)
+		if c == "/" {
+			numSlash++
+		}
+		if numSlash == 2 {
+			rmPrefix = uri[i+1:]
+			break
+		}
+	}
+	for _, r := range rmPrefix {
+		c := string(r)
+		if c == "?" {
+			result += ";"
+		} else {
+			result += c
+		}
+	}
+	return result
 }
