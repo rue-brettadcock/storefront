873725d4ec354de3d53102a465b3afc7b3396a64
Brett Adcock <brettadcock@ruelala.com>
2017-08-22 09:27:10 -0400

    refactored to use error type

--- logic/logic.go
+++ logic/logic.go
@@ -1,6 +1,7 @@
 package logic
 
 import (
+	"errors"
 	"log"
 	"strconv"
 
@@ -14,11 +15,11 @@ type logic struct {
 
 //Logic explicit interface for ioc
 type Logic interface {
-	AddProductSKU(SKU) string
-	UpdateProductQuantity(SKU) string
-	DeleteID(SKU) string
+	AddProductSKU(SKU) error
+	UpdateProductQuantity(SKU) error
+	DeleteID(SKU) error
 	PrintAllProductInfo() string
-	GetProductInfo(SKU) string
+	GetProductInfo(SKU) (string, error)
 }
 
 //SKU for holding product information
@@ -36,52 +37,52 @@ func New() Logic {
 }
 
 //AddProductSKU validates product info and Inserts into the db
-func (l *logic) AddProductSKU(sku SKU) string {
+func (l *logic) AddProductSKU(sku SKU) error {
 	id, _ := strconv.Atoi(sku.ID)
 	quant, _ := strconv.Atoi(sku.Quantity)
-	if l.mydb.Get(id) != "" {
-		return "Product id already exists"
+	if l.mydb.Get(id) != "[]" {
+		return errors.New("Product id already exists")
 	}
 	if quant < 1 {
-		return "Quantity must be at least 1"
+		return errors.New("Quantity must be at least 1")
 	}
 	if id < 0 {
-		return "ID must be positive"
+		return errors.New("ID must be positive")
 	}
 
 	err := l.mydb.Insert(id, sku.Name, sku.Vendor, quant)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return "Product successfully added to database"
+	return nil
 }
 
 //UpdateProductQuantity updates quantity for a given id
-func (l *logic) UpdateProductQuantity(sku SKU) string {
+func (l *logic) UpdateProductQuantity(sku SKU) error {
 	id, _ := strconv.Atoi(sku.ID)
 	quant, _ := strconv.Atoi(sku.Quantity)
-	if l.mydb.Get(id) == "" {
-		return "Product id doesn't exist"
+	if l.mydb.Get(id) == "[]" {
+		return errors.New("Product id doesn't exist")
 	}
 
 	err := l.mydb.Update(id, quant)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return "SKU successfully updated"
+	return nil
 }
 
 //DeleteID removes all product information for a given id
-func (l *logic) DeleteID(sku SKU) string {
+func (l *logic) DeleteID(sku SKU) error {
 	id, _ := strconv.Atoi(sku.ID)
-	if l.mydb.Get(id) == "" {
-		return "Product id doesn't exist"
+	if l.mydb.Get(id) == "[]" {
+		return errors.New("Product id doesn't exist")
 	}
 	err := l.mydb.Delete(id)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return "Product successfully deleted"
+	return nil
 }
 
 //PrintAllProductInfo returns all product SKUs
@@ -90,10 +91,10 @@ func (l *logic) PrintAllProductInfo() string {
 }
 
 //GetProductInfo returns product details for given id
-func (l *logic) GetProductInfo(sku SKU) string {
+func (l *logic) GetProductInfo(sku SKU) (string, error) {
 	id, _ := strconv.Atoi(sku.ID)
-	if l.mydb.Get(id) == "" {
-		return "Product id doesn't exist"
+	if l.mydb.Get(id) == "[]" {
+		return "[]", errors.New("Product id doesn't exist")
 	}
-	return l.mydb.Get(id)
+	return l.mydb.Get(id), nil
 }
