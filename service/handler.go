ff06fa2df86c08170c33f86fb7f127fe915d7be1
Brett Adcock <brettadcock@ruelala.com>
2017-08-22 09:28:20 -0400

    added proper http header responces

--- service/handler.go
+++ service/handler.go
@@ -17,48 +17,63 @@ type Presentation struct {
 func (p *Presentation) addSKU(res http.ResponseWriter, req *http.Request) {
 	var sku logic.SKU
 	json.NewDecoder(req.Body).Decode(&sku)
-	res.Header().Set("Content-Type", "text/plain")
-	res.WriteHeader(http.StatusOK)
 
-	msg := p.logic.AddProductSKU(sku)
-	fmt.Fprintf(res, "%v\n", msg)
+	err := p.logic.AddProductSKU(sku)
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	res.WriteHeader(http.StatusCreated)
 }
 
 func (p *Presentation) printSKUs(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set("Content-Type", "text/plain")
-	res.WriteHeader(http.StatusOK)
+	res.Header().Set("Content-Type", "application/json")
 
 	msg := p.logic.PrintAllProductInfo()
 	fmt.Fprintf(res, "%v\n", msg)
+	if msg == "[]" {
+		res.WriteHeader(http.StatusNoContent)
+		return
+	}
+	res.WriteHeader(http.StatusOK)
 }
 
 func (p *Presentation) getSKU(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+
 	var sku logic.SKU
 	params := mux.Vars(req)
 	sku.ID = params["id"]
-	res.Header().Set("Content-Type", "text/plain")
-	res.WriteHeader(http.StatusOK)
-
-	msg := p.logic.GetProductInfo(sku)
+	msg, err := p.logic.GetProductInfo(sku)
 	fmt.Fprintf(res, "%v\n", msg)
+	if err != nil {
+		res.WriteHeader(http.StatusNoContent)
+		return
+	}
+	res.WriteHeader(http.StatusOK)
 }
 
 func (p *Presentation) updateSKU(res http.ResponseWriter, req *http.Request) {
 	var sku logic.SKU
 	json.NewDecoder(req.Body).Decode(&sku)
-	res.Header().Set("Content-Type", "text/plain")
-	res.WriteHeader(http.StatusOK)
 
-	msg := p.logic.UpdateProductQuantity(sku)
-	fmt.Fprintf(res, "%v\n", msg)
+	err := p.logic.UpdateProductQuantity(sku)
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	res.WriteHeader(http.StatusOK)
 }
 
 func (p *Presentation) deleteSKU(res http.ResponseWriter, req *http.Request) {
 	var sku logic.SKU
-	json.NewDecoder(req.Body).Decode(&sku)
-	res.Header().Set("Content-Type", "text/plain")
-	res.WriteHeader(http.StatusOK)
+	params := mux.Vars(req)
+	sku.ID = params["id"]
 
-	msg := p.logic.DeleteID(sku)
-	fmt.Fprintf(res, "%v\n", msg)
+	err := p.logic.DeleteID(sku)
+	if err != nil {
+		res.WriteHeader(http.StatusNoContent)
+		return
+	}
+	res.WriteHeader(http.StatusOK)
 }
