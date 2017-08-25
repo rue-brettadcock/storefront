f2e890c16437b4ee78088f9ee866c0e4c9818f01
Brett Adcock <brettadcock@ruelala.com>
2017-08-25 09:26:44 -0400

    updated tests to account for sku.id type change

--- database/db-local_test.go
+++ database/db-local_test.go
@@ -7,9 +7,9 @@ import (
 
 func populateSKUs() []sku {
 	var res []sku
-	res = append(res, sku{1, "polo", "CK", 100})
-	res = append(res, sku{2, "pen", "bic", 50})
-	res = append(res, sku{3, "watch", "timex", 25})
+	res = append(res, sku{"1", "polo", "CK", 100})
+	res = append(res, sku{"2", "pen", "bic", 50})
+	res = append(res, sku{"3", "watch", "timex", 25})
 	return res
 }
 
@@ -17,14 +17,14 @@ func TestDelete_multipleElementSlice(t *testing.T) {
 	data := MemDb{db: newConnection()}
 	data.db = populateSKUs()
 
-	err := data.Delete(1)
+	err := data.Delete("1")
 	if err != nil {
 		t.Error(err)
 	}
 
 	var empty []sku
-	expected := append(empty, sku{2, "pen", "bic", 50})
-	expected = append(expected, sku{3, "watch", "timex", 25})
+	expected := append(empty, sku{"2", "pen", "bic", 50})
+	expected = append(expected, sku{"3", "watch", "timex", 25})
 	if !reflect.DeepEqual(data.db, expected) {
 		t.Errorf("Actual: %v\nExpected: %v", data.db, expected)
 	}
@@ -34,7 +34,7 @@ func TestDelete_emptySlice(t *testing.T) {
 	data := MemDb{db: newConnection()}
 	var empty []sku
 
-	err := data.Delete(1)
+	err := data.Delete("1")
 	if err != nil {
 		t.Error(err)
 	}
