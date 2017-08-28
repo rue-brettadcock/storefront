91f7102867f7b8e2f536f66ee1a0f81027c72a81
Brett Adcock <brettadcock@ruelala.com>
2017-08-28 14:52:26 -0400

    added more tests for the local database layer

--- database/db-local_test.go
+++ database/db-local_test.go
@@ -45,6 +45,19 @@ func TestDelete_emptySlice(t *testing.T) {
 	}
 }
 
+func TestDelete_oneElementSlice_Success(t *testing.T) {
+	data := MemDb{db: newConnection()}
+	data.db = append(data.db, sku{"100", "seltzer", "poland springs", 34})
+
+	err := data.Delete("100")
+	if err != nil {
+		t.Error(err)
+	}
+	if len(data.db) != 0 {
+		t.Errorf("Actual: %v\nExpected: %v", len(data.db), 0)
+	}
+}
+
 func TestUpdate_happypath_success(t *testing.T) {
 	data := MemDb{db: newConnection()}
 	data.db = populateSKUs()
@@ -68,8 +81,60 @@ func TestUpdate_idDoesntExist_noIDfound(t *testing.T) {
 	data.db = populateSKUs()
 
 	err := data.Update("5", 2500)
-	expected := errors.New("No matching product id found")
+	expected := errors.New("Failed to update sku")
 	if err.Error() != expected.Error() {
 		t.Errorf("Actual: %v\nExpected: %v", err, expected)
+	}
+}
+
+func TestGet_idExists_success(t *testing.T) {
+	data := MemDb{db: newConnection()}
+	data.db = populateSKUs()
+
+	actual := data.Get("3")
+	expected := `{"id":"3","name":"watch","vendor":"timex","quantity":25}`
+	if actual != expected {
+		t.Errorf("Actual: %v\nExpected: %v", actual, expected)
+	}
+}
+
+func TestGet_idDoesntExists_emptyJSON(t *testing.T) {
+	data := MemDb{db: newConnection()}
+	data.db = populateSKUs()
+
+	actual := data.Get("100")
+	expected := `[]`
+	if actual != expected {
+		t.Errorf("Actual: %v\nExpected: %v", actual, expected)
+	}
+}
+
+func TestPrint_EmptyDB_EmptyJSON(t *testing.T) {
+	data := MemDb{db: newConnection()}
+
+	actual := data.Print()
+	expected := "[]"
+	if actual != expected {
+		t.Errorf("Actual: %v\nExpected: %v", actual, expected)
+	}
+}
+
+func TestPrint_populatedDB_JSONstring(t *testing.T) {
+	data := MemDb{db: newConnection()}
+	data.db = populateSKUs()
+
+	actual := data.Print()
+	expected := `[{"id":"1","name":"polo","vendor":"CK","quantity":100},{"id":"2","name":"pen","vendor":"bic","quantity":50},{"id":"3","name":"watch","vendor":"timex","quantity":25}]`
+	if actual != expected {
+		t.Errorf("Actual: %v\nExpected: %v", actual, expected)
+	}
+}
+
+func TestInsert_emptylist(t *testing.T) {
+	data := MemDb{db: newConnection()}
+
+	err := data.Insert("12345", "iphone", "apple", 720)
+	if err != nil {
+		t.Error(err)
 	}
 }
