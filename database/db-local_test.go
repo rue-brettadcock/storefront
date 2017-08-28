9bf62ac80b72f3c5e1280f3439d978f7c9f11a6d
Brett Adcock <brettadcock@ruelala.com>
2017-08-28 14:12:08 -0400

    added tests for update method

--- database/db-local_test.go
+++ database/db-local_test.go
@@ -3,6 +3,8 @@ package database
 import (
 	"reflect"
 	"testing"
+
+	errors "github.com/pkg/errors"
 )
 
 func populateSKUs() []sku {
@@ -40,5 +42,34 @@ func TestDelete_emptySlice(t *testing.T) {
 	}
 	if !reflect.DeepEqual(data.db, empty) {
 		t.Errorf("Actual: %v\nExpected: %v", data.db, empty)
+	}
+}
+
+func TestUpdate_happypath_success(t *testing.T) {
+	data := MemDb{db: newConnection()}
+	data.db = populateSKUs()
+
+	var expected []sku
+	expected = append(expected, sku{"1", "polo", "CK", 2500})
+	expected = append(expected, sku{"2", "pen", "bic", 50})
+	expected = append(expected, sku{"3", "watch", "timex", 25})
+
+	err := data.Update("1", 2500)
+	if err != nil {
+		t.Errorf("Actual: %v\nExpected: %v", data.db, expected)
+	}
+	if !reflect.DeepEqual(data.db, expected) {
+		t.Errorf("Actual: %v\nExpected: %v", data.db, expected)
+	}
+}
+
+func TestUpdate_idDoesntExist_noIDfound(t *testing.T) {
+	data := MemDb{db: newConnection()}
+	data.db = populateSKUs()
+
+	err := data.Update("5", 2500)
+	expected := errors.New("No matching product id found")
+	if err.Error() != expected.Error() {
+		t.Errorf("Actual: %v\nExpected: %v", err, expected)
 	}
 }
