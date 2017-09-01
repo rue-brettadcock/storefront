fcff4d5b3244a504c308140b946c611e403bd0a3
Brett Adcock <brettadcock@ruelala.com>
2017-09-01 11:43:10 -0400

    use in memory db

--- logic/logic.go
+++ logic/logic.go
@@ -35,7 +35,7 @@ type SKU struct {
 func New() Logic {
 	//database.NewSQL() to use mySQL db
 	//database.NewInMemoryDB() to use local db
-	l := logic{mydb: database.NewSQL()}
+	l := logic{mydb: database.NewInMemoryDB()}
 	return &l
 }
 
